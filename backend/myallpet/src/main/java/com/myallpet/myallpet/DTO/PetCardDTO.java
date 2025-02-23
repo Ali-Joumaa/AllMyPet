372585e56a211d74f4459099d7d4a39385cc570b
Ali-Joumaa <alijoumaa2003@gmail.com>
2025-02-24 00:23:31 +0200

    Adding DTO Files

---
+++ backend/myallpet/src/main/java/com/myallpet/myallpet/DTO/PetCardDTO.java
@@ -0,0 +1,20 @@
+package com.myallpet.myallpet.DTO;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PetCardDTO {
+    private Integer petId;
+    private String name;
+    private String species;
+    private String breed;
+    private Integer age;
+    private String sex;
+    private String petPhoto;
+    private String description;
+    private String location;
+    private String status;
+}
