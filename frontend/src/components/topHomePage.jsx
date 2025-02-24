d68d951e8b57a522610d7af509a8e0c7c0bf72a8
Fathy Ibrahim <fai08@mail.aub.edu>
2025-02-24 22:34:47 +0200

    Added new components: (PetCare, ReviewCards & ReviewSection, TopHomePage) and made them all responsive

---
+++ frontend/src/components/topHomePage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./topHomePage.css";
+import DogAndCatImage from "../images/DogAndCat.svg";
+
+export default function topHomePage() {
+  return (
+    <div className="top-home-container">
+      <div className="top-home-text">
+        <h1>
+        Give a Pet a <br />
+        <span className="loving-text">
+            Loving <span style={{ color: "#2E256F" }}>Home</span>
+        </span>
+        </h1>
+
+        <p>
+          Looking for a furry companion? Many adorable pets are waiting for a forever home. 
+          Browse through our adoption listings and find the perfect match for your family.
+           Adopting a pet not only brings joy to your home but also saves a life.
+        </p>
+        <div className="top-home-buttons">
+          <button className="adopt-btn">Adopt Now</button>
+          <button className="raise-btn">Raise a Pet</button>
+        </div>
+      </div>
+      <div className="top-home-image">
+        <img src={DogAndCatImage} alt="Dog and Cat" />
+      </div>
+    </div>
+  );
+}
